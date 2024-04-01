e00d1bedb177385038f22717a88147ae9288edee
amitaih73 <amitai.adler@gmail.com>
2024-04-01 14:14:03 +0300

    make a define for BORD =std::vector<char>

--- inc/bord.hpp
+++ inc/bord.hpp
@@ -3,19 +3,22 @@
 
 namespace xo{
 
+using BORD = std::vector<std::vector<char>>;
+
 class Bord final{
 public:
     explicit Bord(int a_dimentions = 3);
     Bord(Bord const& a_other) = default;
     Bord& operator=(Bord const& a_other) = delete;
     ~Bord() = default;
+
     char const& get(int a_row, int a_colo) noexcept;
     void set(int a_row, int a_colo, char a_toSet) noexcept;
-    void print() noexcept;
     int dimention() noexcept;
+    void print() noexcept;
 
 private:
-    std::vector<std::vector<char>> m_bord; 
+    BORD m_bord; 
     int m_dimention;
 };
 
